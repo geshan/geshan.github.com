b07a74ff15a18f49e7d7cb41e825d3f387d6194b
Geshan Manandhar <geshan.manandhar@theiconic.com.au>
2020-04-13 15:54:09 +1000

    Edit reading time to match medium and dev.to @ 250 wmp

--- _data/helpers.js
+++ _data/helpers.js
@@ -4,7 +4,7 @@ function wordCount(s) {
 
 module.exports = {
   getReadingTime(text) {
-    const wordsPerMinute = 225;
+    const wordsPerMinute = 250;
     const numberOfWords = text.split(/\s/g).length;
     return Math.ceil(numberOfWords / wordsPerMinute);
   },
