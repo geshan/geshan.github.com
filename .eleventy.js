d82efc593d7868b6ba20a59ae363334d72274267
Geshan Manandhar <geshan.manandhar@theiconic.com.au>
2020-04-21 17:01:09 +1000

    Use iso date for sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -39,7 +39,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addFilter("getSitemapDate", dateObj => {
     const dateInUTC = DateTime.fromJSDate(dateObj, {zone: 'utc'});
-    return dateInUTC.toISO();
+    return dateInUTC.toISODate();
   });
 
   eleventyConfig.addFilter("yearMonth", dateObj => {
