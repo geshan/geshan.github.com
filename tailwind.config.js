b83383a3dbf5c3a58051e2d9221f611c96b8f075
Geshan Manandhar <geshan@simplywallst.com>
2022-06-23 15:52:34 +1000

    Minor fixes for basic tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: [
-    './**/*.html', './**/**/*.html', './**/**/*.html'
+    './**/*.html', './**/**/*.html', './**/**/*.html', './**/**/**/*.html'
   ],
   theme: {
     extend: {},
