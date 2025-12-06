703e007b88b682cd970e7b22ed663b2d72d8f068
Geshan Manandhar <geshan@gmail.com>
2025-12-06 15:02:11 +1100

    Fix popular posts and some npm libraries' versions

--- _data/popular.js
+++ _data/popular.js
@@ -2,8 +2,12 @@ const axios = require('axios');
 
 async function getPopularPosts() {
   try {
-    const response = await axios.get(process.env.GA_API_ENDPOINT);
-    return response.data;
+    if (process.env.GA_API_ENDPOINT) {
+      const response = await axios.get(process.env.GA_API_ENDPOINT);
+      return response.data;
+    }
+    
+    return [];    
   } catch (e) {
     console.log(`Error getting popular posts: `, e);
     return [];
